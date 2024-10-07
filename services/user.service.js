174b8bc6d212e3e2edaa40210473e10fe14140c8
Nicolas Chirino <nicolas.brch@gmail.com>
2024-10-06 22:24:32 -0300

    Fix promises to correctly use await on calls

--- services/user.service.js
+++ services/user.service.js
@@ -1,9 +1,27 @@
-import db from "utils/db.js"
+import db from "../utils/db.js"
 
-export const createUser = async (username, hashedPassword) => {
-  return await db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword]);
-}
+export const createUser = (username, hashedPassword) => {
+  return new Promise((resolve, reject) => {
+    db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword], function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this.lastID);
+      }
+    });
+  });
+};
 
-export const getUser = async (username) => {
-  return await db.get('SELECT * FROM users WHERE username = ?', [username]);
-}
+export const getUser = (username) => {
+  return new Promise((resolve, reject) => {
+    db.get('SELECT * FROM users WHERE username = ?', [username], (err, user) => {
+      if (err) {
+        reject(err);
+      } else if (!user) {
+        resolve(null);
+      } else {
+        resolve(user);
+      }
+    });
+  });
+};
