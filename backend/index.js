39efc6495fd7cebec8748ef222f607bf5112ea5f
Nico <nicolas.brch@gmail.com>
2024-11-10 23:36:46 -0300

    Ayudantía 08 Re-estructurar archivos a backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
 import { neon } from '@neondatabase/serverless';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { cartRouter } from "./routes/cart.js";
 import { productRouter } from "./routes/product.js";
+import { userRouter } from "./routes/user.js";
 
 const dbString = process.env.DATABASE_URL;
 if (!dbString) {
@@ -25,33 +24,10 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Docs
-const swaggerSpecs = swaggerJSDoc({
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Example API',
-      version: '1.0.0',
-      description: 'API docs',
-    },
-    components: {
-      securitySchemes: {
-        cookieAuth: {
-          type: 'apiKey',
-          in: 'cookie',
-          name: 'token',
-        },
-      },
-    },
-  },
-  apis: ['./routes/*.js'],
-});
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-
 // Routes
 app.use(cartRouter);
 app.use(productRouter);
+app.use(userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} 🚀`);
